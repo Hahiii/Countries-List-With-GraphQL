4f84d015387b5a0e78680feea02aee99965b084e
Nazim Shala <shalanazim@gmail.com>
2020-06-09 17:07:00 +0200

    add fetch and render countires on page

--- src/components/countries/index.js
+++ src/components/countries/index.js
@@ -1,11 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
 function Countries() {
+  const [toggle, setToggle] = useState("closed");
+  const [countries, setCountries] = useState();
+  const countriesQuery = `{
+    countries {
+        name
+        emoji
+    }
+  }`;
+  const baseURL = "https://countries.trevorblades.com/";
   useEffect(() => {
-    console.log("helllo");
-  }, []);
-  return <ul className="countries-container"></ul>;
+    async function getCountries() {
+      const { data } = await fetch(baseURL, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          query: countriesQuery,
+        }),
+      }).then((res) => {
+        return res.json();
+      });
+      setCountries(data);
+    }
+    getCountries();
+  }, [countriesQuery]);
+  return (
+    <ul className="countries-list-container">
+      {countries &&
+        countries.countries.map((country) => {
+          return (
+            <li
+              className="countries-list__item"
+              data-toggle={toggle}
+              key={country.name}
+            >
+              <h2 className="countries-list__item__headline">
+                <span className="countries-list__item__icon">
+                  {country.emoji}
+                </span>
+                {country.name}
+              </h2>
+            </li>
+          );
+        })}
+    </ul>
+  );
 }
 
 export default Countries;
